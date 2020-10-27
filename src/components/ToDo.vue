948da51384d17641c1a451c577a8cc5a47b0820f
code-in-time@hotmail.com <andrew.crawford.za@gmail.com>
2020-10-27 18:49:14 +0000

    Add click event an use a table

--- src/components/ToDo.vue
+++ src/components/ToDo.vue
@@ -1,32 +1,37 @@
 <template>
-  <ul class="list-group">
-    {{username}}
-    <!-- <li class="list-group-item">Cras justo odio <button type="button" class="btn btn-primary">select</button></li> -->
-
-    <li v-for="item in allTodos" :key="item.userId" class="list-group-item">
-      {{ item.completed }} | {{ item.title }} | {{ item.userId }}
-      Cras justo odio <button type="button" class="btn btn-primary">select</button>
-    </li>
-
-  </ul>
+  <table class="table">
+    <thead>
+      <tr>
+        <th>userId</th>
+        <th>title</th>
+        <th>completed</th>
+        <th>button</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="item in allTodos" :key="item.id+item.title">
+        <td>{{ item.userId }}</td>
+        <td>{{ item.title }}</td>
+        <td>{{ item.completed }}</td>
+        <td><button v-on:click="say(item.userId)" type="button" class="btn btn-primary">select</button></td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 
 <script>
 export default {
   name: 'ToDo',
     props: ["allTodos"],
-  // props: {
-  //   msg: String,
-  //   username: String,
-  //     props: ["allTodos"],
-  // }
+    methods: {
+    say: function (userId) {
+      console.log(userId)
+    }
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  .container {
-    width: 200px;
-    display: "inline-block";
-  }
+
 </style>
