52d8039ba70ee7c53c1704bb8a0e1a2afba895de
code-in-time@hotmail.com <andrew.crawford.za@gmail.com>
2020-10-27 17:38:52 +0000

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <div class="row">
+      <div class="col">
+        <ToDo />
+      </div>
+      <div class="col">
+        <ToDo />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ToDo from './components/ToDo.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ToDo
   }
 }
 </script>
@@ -25,4 +31,6 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+
 </style>
---
+++ src/components/ToDo.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul class="list-group">
+    <li class="list-group-item">Cras justo odio <button type="button" class="btn btn-primary">select</button></li>
+    <li class="list-group-item">Cras justo odio <button type="button" class="btn btn-primary">select</button></li>
+    <li class="list-group-item">Cras justo odio <button type="button" class="btn btn-primary">select</button></li>
+    <li class="list-group-item">Cras justo odio <button type="button" class="btn btn-primary">select</button></li>
+    <li class="list-group-item">Cras justo odio <button type="button" class="btn btn-primary">select</button></li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .container {
+    width: 200px;
+    display: "inline-block";
+  }
+</style>
